820501a7bfec42c7c68e685d937d5afe885f0176
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2024-03-02 22:18:56 +0000

    swagger

--- crop_disease_app/Program.cs
+++ crop_disease_app/Program.cs
@@ -31,10 +31,8 @@ var app = builder.Build();
 app.UseCors();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
 
