bda56019e3eda37be5775fb4ed072410a15f7133
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2024-03-03 08:52:24 +0000

    fix alert

--- crop_disease_app/Models/Alert.cs
+++ crop_disease_app/Models/Alert.cs
@@ -1,7 +1,8 @@
 namespace hackathon_template.Models; 
 
-public class Alert(long timestamp, double latitude, double longitude, string pest) {
-    public string PostedBy { get; set; }
+public class Alert(int id, string postedBy, long timestamp, double latitude, double longitude, string pest) {
+    public int Id { get; init; } = id;
+    public string PostedBy { get; set; } = postedBy;
     public long Timestamp { get; set; } = timestamp;
     public double Latitude { get; set; } = latitude;
     public double Longitude { get; set; } = longitude;
