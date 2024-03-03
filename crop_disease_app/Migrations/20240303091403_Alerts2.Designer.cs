56e38236a9c3eb2dcf8c8e523bd7406f9171c4b0
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2024-03-03 09:14:24 +0000

    mig

--- crop_disease_app/Migrations/20240303091403_Alerts2.Designer.cs
+++ crop_disease_app/Migrations/20240303091403_Alerts2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using hackathon_template.Config;
@@ -12,9 +13,11 @@ using hackathon_template.Config;
 namespace hackathon_template.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240303091403_Alerts2")]
+    partial class Alerts2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ crop_disease_app/Migrations/20240303091403_Alerts2.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hackathon_template.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alerts2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserSettings_ShareLocation",
+                table: "AspNetUsers",
+                newName: "userSettings_ShareLocation");
+
+            migrationBuilder.RenameColumn(
+                name: "UserSettings_Longitude",
+                table: "AspNetUsers",
+                newName: "userSettings_Longitude");
+
+            migrationBuilder.RenameColumn(
+                name: "UserSettings_Latitude",
+                table: "AspNetUsers",
+                newName: "userSettings_Latitude");
+
+            migrationBuilder.RenameColumn(
+                name: "UserSettings_GetEmailAlerts",
+                table: "AspNetUsers",
+                newName: "userSettings_GetEmailAlerts");
+
+            migrationBuilder.RenameColumn(
+                name: "UserSettings_AlertsFromInDays",
+                table: "AspNetUsers",
+                newName: "userSettings_AlertsFromInDays");
+
+            migrationBuilder.RenameColumn(
+                name: "UserSettings_AlertRadius",
+                table: "AspNetUsers",
+                newName: "userSettings_AlertRadius");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "userSettings_ShareLocation",
+                table: "AspNetUsers",
+                newName: "UserSettings_ShareLocation");
+
+            migrationBuilder.RenameColumn(
+                name: "userSettings_Longitude",
+                table: "AspNetUsers",
+                newName: "UserSettings_Longitude");
+
+            migrationBuilder.RenameColumn(
+                name: "userSettings_Latitude",
+                table: "AspNetUsers",
+                newName: "UserSettings_Latitude");
+
+            migrationBuilder.RenameColumn(
+                name: "userSettings_GetEmailAlerts",
+                table: "AspNetUsers",
+                newName: "UserSettings_GetEmailAlerts");
+
+            migrationBuilder.RenameColumn(
+                name: "userSettings_AlertsFromInDays",
+                table: "AspNetUsers",
+                newName: "UserSettings_AlertsFromInDays");
+
+            migrationBuilder.RenameColumn(
+                name: "userSettings_AlertRadius",
+                table: "AspNetUsers",
+                newName: "UserSettings_AlertRadius");
+        }
+    }
+}
