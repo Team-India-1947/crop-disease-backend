ce4588a4e45aa24207a3857f336d647769a83277
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2024-03-03 08:54:03 +0000

    migration

---
+++ crop_disease_app/Migrations/20240303085350_Alerts.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace hackathon_template.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alerts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "UserSettings_AlertRadius",
+                table: "AspNetUsers",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserSettings_AlertsFromInDays",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UserSettings_GetEmailAlerts",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<double>(
+                name: "UserSettings_Latitude",
+                table: "AspNetUsers",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "UserSettings_Longitude",
+                table: "AspNetUsers",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UserSettings_ShareLocation",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Alerts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PostedBy = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<long>(type: "bigint", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    Pest = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alerts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Diseases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    DiseaseName = table.Column<string>(type: "text", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diseases", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alerts");
+
+            migrationBuilder.DropTable(
+                name: "Diseases");
+
+            migrationBuilder.DropColumn(
+                name: "UserSettings_AlertRadius",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserSettings_AlertsFromInDays",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserSettings_GetEmailAlerts",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserSettings_Latitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserSettings_Longitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserSettings_ShareLocation",
+                table: "AspNetUsers");
+        }
+    }
+}
